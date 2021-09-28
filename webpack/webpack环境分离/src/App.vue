90311a8765618666e7cef0095d4c9ae068d56a24
yanshijing <yanshijing@comjia.com>
2021-09-28 09:58:18 +0800

    webpack配置文件环境分离

---
+++ webpack/webpack环境分离/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="user-center">
+    <h2>{{username}}</h2>
+    <p>{{age}}</p>
+    <p>{{height}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      username: "Nicholas Zakas",
+      age: 18,
+      height: 1.88
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
