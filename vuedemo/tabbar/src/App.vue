1a4a83770825a15920e8c62d0c5998b5143722ac
yanshijing <yanshijing@comjia.com>
2021-10-16 21:25:40 +0800

    tabbar的组件继续抽离、封装

--- vuedemo/tabbar/src/App.vue
+++ vuedemo/tabbar/src/App.vue
@@ -1,55 +1,16 @@
 <template>
   <div id="app">
     <router-view />
-    <tabbar>
-      <tab-bar-item path="/home" activeColor="green">
-        <img slot="item-icon" src="./assets/images/tabbar/img1.png" alt="" />
-        <img
-          slot="item-icon-active"
-          src="./assets/images/tabbar/img1-active.png"
-          alt=""
-        />
-        <span slot="item-text">首页</span>
-      </tab-bar-item>
-      <tab-bar-item path="/category">
-        <img slot="item-icon" src="./assets/images/tabbar/img2.png" alt="" />
-        <img
-          slot="item-icon-active"
-          src="./assets/images/tabbar/img2-active.png"
-          alt=""
-        />
-        <span slot="item-text">分类</span>
-      </tab-bar-item>
-      <tab-bar-item path="/shopcar">
-        <img slot="item-icon" src="./assets/images/tabbar/img3.png" alt="" />
-        <img
-          slot="item-icon-active"
-          src="./assets/images/tabbar/img3-active.png"
-          alt=""
-        />
-        <span slot="item-text">购物车</span>
-      </tab-bar-item>
-      <tab-bar-item path="/profile">
-        <img slot="item-icon" src="./assets/images/tabbar/img4.png" alt="" />
-        <img
-          slot="item-icon-active"
-          src="./assets/images/tabbar/img4-active.png"
-          alt=""
-        />
-        <span slot="item-text">我的</span>
-      </tab-bar-item>
-    </tabbar>
+    <main-tabbar></main-tabbar>
   </div>
 </template>
 
 <script>
-import Tabbar from "./components/tabbar/tabbar.vue";
-import TabBarItem from "./components/tabbar/TabBarItem.vue";
+import MainTabbar from "./components/tabbar/MainTabbar.vue";
 export default {
   name: "App",
   components: {
-    Tabbar,
-    TabBarItem,
+    MainTabbar,
   },
 };
 </script>
---
+++ vuedemo/tabbar/src/components/tabbar/MainTabbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <tarbar>
+    <tab-bar-item path="/home" activeColor="green">
+      <img slot="item-icon" src="../../assets/images/tabbar/img1.png" alt="" />
+      <img
+        slot="item-icon-active"
+        src="../../assets/images/tabbar/img1-active.png"
+        alt=""
+      />
+      <span slot="item-text">首页</span>
+    </tab-bar-item>
+    <tab-bar-item path="/category" activeColor="#369">
+      <img slot="item-icon" src="../../assets/images/tabbar/img2.png" alt="" />
+      <img
+        slot="item-icon-active"
+        src="../../assets/images/tabbar/img2-active.png"
+        alt=""
+      />
+      <span slot="item-text">分类</span>
+    </tab-bar-item>
+    <tab-bar-item path="/shopcar">
+      <img slot="item-icon" src="../../assets/images/tabbar/img3.png" alt="" />
+      <img
+        slot="item-icon-active"
+        src="../../assets/images/tabbar/img3-active.png"
+        alt=""
+      />
+      <span slot="item-text">购物车</span>
+    </tab-bar-item>
+    <tab-bar-item path="/profile">
+      <img slot="item-icon" src="../../assets/images/tabbar/img4.png" alt="" />
+      <img
+        slot="item-icon-active"
+        src="../../assets/images/tabbar/img4-active.png"
+        alt=""
+      />
+      <span slot="item-text">我的</span>
+    </tab-bar-item>
+  </tarbar>
+</template>
+
+<script>
+import Tarbar from "./tabbar.vue";
+import TabBarItem from "./TabBarItem.vue";
+export default {
+  components: {
+    Tarbar,
+    TabBarItem,
+  },
+};
+</script>
