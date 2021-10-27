580453d2969e8c6d7ba0c61adbd2c04985ad4318
suchcl <ya5969@163.com>
2021-10-27 09:16:22 +0800

    v-for和v-if嵌套、通过计算属性解决嵌套问题的案例

--- vuedemo/vuerouterpro1/src/components/user.vue
+++ vuedemo/vuerouterpro1/src/components/user.vue
@@ -1,24 +1,32 @@
 <template>
-  <div class="user">
-    <h2>用户列表</h2>
-    <table></table>
-    <tr>
-      <th>编号</th>
-      <th>姓名</th>
-      <th>年龄</th>
-    </tr>
-    <tr v-for="item in userinfo" :key="item.id">
-      <td>{{ item.id }}</td>
-      <td>
-        <!--params方式传参-->
-        <router-link :to="'/user/' + item.id">
-          {{ item.username }}
-        </router-link>
-      </td>
-      <td>
-        {{ item.age }}
-      </td>
-    </tr>
+  <div>
+    <div class="user">
+      <h2>用户列表</h2>
+      <table></table>
+      <tr>
+        <th>编号</th>
+        <th>姓名</th>
+        <th>年龄</th>
+      </tr>
+      <tr v-for="item in userinfo" :key="item.id">
+        <td>{{ item.id }}</td>
+        <td>
+          <!--params方式传参-->
+          <router-link :to="'/user/' + item.id">{{ item.username }}</router-link>
+        </td>
+        <td>{{ item.age }}</td>
+      </tr>
+    </div>
+    <div class="test-user">
+      <ul>
+        <!-- v-for和v-if不要同时使用 -->
+        <!-- <li v-for="item in userinfo" :key="item.id" v-if="item.isActive">{{ item.username }}</li> -->
+        <!-- <li v-for="item in userinfo" :key="item.id">
+          <span v-if="item.isActive">{{ item.username }}</span>
+        </li>-->
+        <li v-for="item in activeUsers" :key="item.id">{{ item.username }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -29,6 +37,21 @@ export default {
     return {
       userinfo: data
     };
+  },
+  computed: {
+    activeUsers() {
+      return this.userinfo.filter((item) => {
+        return item.isActive;
+      });
+    }
   }
 };
 </script>
+
+<style scoped>
+ul,
+li,
+ol {
+  list-style: none;
+}
+</style>
