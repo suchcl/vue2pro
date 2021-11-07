b7bfd791373dc5af9caf98a8c93626af4070ab19
suchcl <ya5969@163.com>
2021-11-07 22:19:27 +0800

    sleep

--- js基础/js/o2.js
+++ js基础/js/o2.js
@@ -1,1 +1,17 @@
-var tt = "LiSongfeng";
+// var tt = "LiSongfeng";
+
+function add(a,b){
+    return a+b;
+}
+
+console.log(typeof add); // function
+
+const obj = {};
+
+Object.defineProperty(obj,"info",{
+    name:"Nicholas Zakas",
+    age: 18,
+    height: 1.98
+});
+
+console.log(obj);
