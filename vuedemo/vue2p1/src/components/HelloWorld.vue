d33440ce5134b06fb0850dc1e7df24071456b09e
suchcl <ya5969@163.com>
2021-12-02 16:58:41 +0800

    小案例

--- vuedemo/vue2p1/src/components/HelloWorld.vue
+++ vuedemo/vue2p1/src/components/HelloWorld.vue
@@ -2,101 +2,65 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
+    <ul class="list">
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <div class="num">
+      <h3>数字列表</h3>
+      <ul class="num-list">
+        <li v-for="(item, index) in num" :key="index">{{ item }}</li>
+      </ul>
+    </div>
+    <button @click="changeOrder">调整顺序</button>
+    <button @click="getVersion">获取版本号</button>
+    <button @click="setVresion">设置版本号</button>
   </div>
 </template>
 
 <script>
+/**
+ * requie:导入的是一个值的引用，使用导入后的值和原来数据已经没有什么关系了
+ * import:导入的是对原数据的引用，导入后的数据修改了，原数据也会跟着修改
+ */
+// const version = import('../../package.json');
+import Version from "../../package.json";
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: "Welcome to Your Vue.js App",
+      vn: {
+        tag: "ul",
+        children: [
+          { tag: "li", children: [{ vnode: { text: 1 } }] },
+          { tag: "li", children: [{ vnode: { text: 2 } }] },
+          { tag: "li", children: [{ vnode: { text: 3 } }] }
+        ]
+      },
+      num: [1, 2, 3]
+    };
+  },
+  methods: {
+    getVersion() {
+      console.log(Version.version);
+    },
+    setVresion() {
+      Version.version = "1.1.2";
+    },
+    changeOrder() {
+      console.log("调整顺序");
+      this.num.reverse();
     }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
@@ -109,5 +73,10 @@ li {
 }
 a {
   color: #42b983;
+}
+.num {
+  background-color: #42b983;
+  padding: 20px;
+  margin-bottom: 20px;
 }
 </style>
